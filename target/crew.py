cf8285d83dac8f1f5839e6cb2a53871b785b4714
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-04-30 08:29:06 -0400

    2025.4.0

---
+++ target/crew.py
@@ -0,0 +1,39 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool
+@CrewBase
+class LatestAiDevelopment:
+	"""Latest AI Development"""
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+	@agent
+	def researcher(self) -> Agent:
+		return Agent(
+		    config=self.agents_config['researcher'],
+		    verbose=True,
+		    tools=[SerperDevTool()]
+		)
+	@agent
+	def reporting_analyst(self) -> Agent:
+		return Agent(
+		    config=self.agents_config['reporting_analyst'],
+		    verbose=True
+		)
+	@task
+	def research_task(self) -> Task:
+		return Task(
+		    config=self.tasks_config['research_task']
+		)
+	@task
+	def reporting_task(self) -> Task:
+		return Task(
+		    config=self.tasks_config['reporting_task']
+		)
+	@crew
+	def crew(self) -> Crew:
+		return Crew(
+			agents=self.agents,
+			tasks=self.tasks,
+			process=Process.sequential,
+			verbose=True
+		)
